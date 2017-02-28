4e04171a939e4b2f0f035c06fe104d703f5f2927
张广上 <gszhang@luopan88.com>
2017-02-28 14:31:08 +0800

    响应式页面搭建完成-v1.0.0

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/**
+ * @desc
+ * @author zhangguangshang on 2017/2/27.
+ * @update zhangguangshang on 2017/2/27.
+ */
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
